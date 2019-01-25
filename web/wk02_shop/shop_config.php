05cae05f0f9c2e44fc091389f6daa5c66ab88c0e
Ali Cope <35251289+Cope-Ali@users.noreply.github.com>
2019-01-25 10:45:08 -0800

    updated configuration and head pages

---
+++ web/wk02_shop/shop_config.php
@@ -0,0 +1,15 @@
+<?php
+	switch ($_SERVER["SCRIPT_NAME"]) {
+		case "browse.php":
+			$CURRENT_PAGE = "Browse"; 
+			$PAGE_TITLE = "Browse - Find the chicken of your DREAMS!";
+			break;
+		case "shoppingcart.php":
+			$CURRENT_PAGE = "Cart"; 
+			$PAGE_TITLE = "Your Shopping Cart - Great Choice!";
+			break;
+		default:
+			$CURRENT_PAGE = "Checkout";
+			$PAGE_TITLE = "Checkout - Make them YOURS!";
+	}
+?>
