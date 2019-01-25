7e358651cc6ff2f06d61c3ca084f63288898d445
Ali Cope <35251289+Cope-Ali@users.noreply.github.com>
2019-01-25 14:22:58 -0800

    bugfix try 2

--- web/wk02_shop/shoppingcart.php
+++ web/wk02_shop/shoppingcart.php
@@ -2,8 +2,9 @@
 session_start();
 echo "Shopping Cart";
 if(isset($_SESSION['userName'])){
-print_r($_SESSION);
-foreach($cartContents as $item)
-echo $item;
+    print_r($_SESSION);
+    foreach($cartContents as $item){
+        echo $item;
+    }
 }
 ?>
