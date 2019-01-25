ac32c3f175200a497378f0127823f66cbf6103aa
Ali Cope <35251289+Cope-Ali@users.noreply.github.com>
2019-01-25 10:54:38 -0800

    add some style to header

--- web/wk02_shop/shop_head.php
+++ web/wk02_shop/shop_head.php
@@ -1,2 +1,20 @@
 <title><?php print $PAGE_TITLE;?></title>
+<header>
 <h1> Country Chickens </h1>
+<h2> Your #1 Source for EGGcellent Chickens!</h2>
+</header>
+
+
+<style>
+
+header {
+    background-color: blue;
+    color: white;
+    padding: 25px;
+    text-align: center;
+    border-bottom: 5px white solid;
+    margin-bottom: 10px;
+}
+
+
+</style>
