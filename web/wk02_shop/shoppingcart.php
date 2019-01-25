d6ccab694ed0bfaeb24e94b555a349b014278549
Ali Cope <35251289+Cope-Ali@users.noreply.github.com>
2019-01-25 15:03:40 -0800

    fixed cart array issue

--- web/wk02_shop/shoppingcart.php
+++ web/wk02_shop/shoppingcart.php
@@ -2,7 +2,7 @@
 session_start();
 echo "Shopping Cart";
 if(isset($_SESSION['userName'])){
-    print_r($_SESSION);
+
 }
 for ($i = 0; $i < count($_SESSION['cart']); $i++){
     echo $_SESSION['cart'][$i] . '<br>';
