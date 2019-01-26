ad3c6a07dc5142dd71bc7ed7d3c66623f95e8f9a
Ali Cope <35251289+Cope-Ali@users.noreply.github.com>
2019-01-26 09:59:49 -0800

    update shopping cart

--- web/wk02_shop/shoppingcart.php
+++ web/wk02_shop/shoppingcart.php
@@ -3,15 +3,40 @@ session_start();
 print_r($_SESSION);
 echo "Shopping Cart";
 if(isset($_SESSION['userName'])){
+
+$frau_qty=0;
+echo $frau_qty;
+$snow_qty=0;
+echo $snow_qty;
+$houdini_qty = 0;
+echo $houdini_qty;
+$roxi_qty = 0;
+echo $roxi_qty;
+
+foreach ($_SESSION["cart"] as $key => $value) {
+    switch($value) {
+        case "frau":
+            $frau_qty++;
+            break;
+        case "snow":
+            $snow_qty++;
+            break;
+        case "houdini":
+            $houdini_qty++;
+            break;
+        case "roxi":
+            $roxi_qty++;
+            break;
+    }
 }
 
-function delete($i)
-{
-  unset($_SESSION['cart'][$i]);
+echo $frau_qty;
+echo $snow_qty;
+echo $houdini_qty;
+echo $roxi_qty;
+
 }
-echo $_POST["frau"];
-if($_POST["frau"] > 0){
-    echo "Frau more than one";}
+
 ?>
 
 <!DOCTYPE html>
@@ -26,32 +51,36 @@ if($_POST["frau"] > 0){
 
 
 <?php
-if($_SESSION["frau"]=="true")
+if($frau_qty>0)
 {
     echo '<p> Frau Flufferbutt </p>';
    echo  '<img src="images/frau.jpg" alt="Very fluffy small black and white chicken" height="50">';
+  echo 'Total in Cart :' . $frau_qty . '<br>';
    echo ' <button type="button" onclick="$_SESSION["frau"]="false""> Delete From Cart </button>';
 }
 
-if($_SESSION["princess"]=="true")
+if($snow_qty>0)
 {
     echo '<p> Princess Snow White </p>
-    <img src="images/snow_white.jpg" alt="Very fluffy small black and white chicken" height="50">
-    <button type="button" onclick="$_SESSION["princess"]="false""> Delete From Cart </button>';
+    <img src="images/snow_white.jpg" alt="Very fluffy small black and white chicken" height="50">';
+    echo 'Total in Cart :' . $snow_qty . '<br>';
+    echo '<button type="button" onclick="$_SESSION["princess"]="false""> Delete From Cart </button>';
 }
 
-if($_SESSION["houdini"]=="true")
+if($houdini_qty>0)
 {
     echo '<p> Houdini </p>
-    <img src="images/houdini.jpg" alt="Very fluffy small black and white chicken" height="50">
-    <button type="button" onclick="$_SESSION["houdini"]="false""> Delete From Cart </button>';
+    <img src="images/houdini.jpg" alt="Very fluffy small black and white chicken" height="50">';
+    echo 'Total in Cart :' . $houdini_qty . '<br>';
+    echo '<button type="button" onclick="$_SESSION["houdini"]="false""> Delete From Cart </button>';
 }
 
-if($_SESSION["roxi"]=="true")
+if($roxi_qty>0)
 {
     echo '<p> Roxi </p>
-    <img src="images/roxi.jpg" alt="Very fluffy small black and white chicken" height="50">
-    <button type="button" onclick="$_SESSION["roxi"]="false""> Delete From Cart </button>';
+    <img src="images/roxi.jpg" alt="Very fluffy small black and white chicken" height="50">';
+    echo 'Total in Cart :' . $roxi_qty . '<br>';
+    echo '<button type="button" onclick="$_SESSION["roxi"]="false""> Delete From Cart </button>';
 }
 ?>
 </body>
