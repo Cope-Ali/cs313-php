102093cef37780e49e5eefa1586fd79fa887b3d9
Ali Cope <35251289+Cope-Ali@users.noreply.github.com>
2019-02-01 14:56:09 -0800

    initial set up oppCenter site

---
+++ web/oppCenter/header.php
@@ -0,0 +1,32 @@
+<header>
+<img src="img/logo.jpg" alt="logo for Chruch of Jesus Christ of Latter-day Saints">
+<h2>Anderson California Stake</h2>
+<h1> Opportunity Center </h1>
+
+</header>
+
+<link href="https://fonts.googleapis.com/css?family=Open+Sans|Poppins:400,600" rel="stylesheet">
+
+<style>
+
+header {
+    background-color: #1A8FB0;
+    Font-family: serif;
+    color: white;
+    padding: 25px;
+    text-align: center;
+    margin: auto;
+}
+
+h1 {
+    font-family: 'Poppins', sans-serif;    
+}
+
+img {
+    float: left;
+}
+
+h2 {
+    font-family: 'Open Sans', sans-serif;
+}
+</style>
---
+++ web/oppCenter/home.php
@@ -0,0 +1,27 @@
+
+<?php
+//start session to store credentials
+session_start();
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <?php include("header.php");?>
+    
+    <link rel="stylesheet" href="oppStyle.css">
+</head>
+<body>
+
+<?php include("nav.php");?>
+
+<div class="container" >
+
+</div>
+
+<?php include("footer.php");?>
+
+</body>
+</html>
