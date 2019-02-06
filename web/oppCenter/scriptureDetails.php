9b251d42730d465360e8fd755f4f6ecbc7d0b676
Ali Cope <35251289+Cope-Ali@users.noreply.github.com>
2019-02-05 21:49:38 -0800

    stretch challenge 2

--- web/oppCenter/scriptureDetails.php
+++ web/oppCenter/scriptureDetails.php
@@ -1,4 +1,3 @@
-
 <?php
 //start session to store credentials
 session_start();
@@ -39,21 +38,13 @@ catch (PDOException $ex)
 
 <div class="container" >
 
-
-<form action="" method="post">
-
-Book: <input type="text" name="book">
-<input type="submit" name="submit" value="Submit">
-</form>
-
-
 <?php
-if($_POST['book'] != "")
+if($_GET['id'] != "")
 {
-    foreach ($db->query('SELECT * FROM scriptures WHERE scriptures_book =\'' . $_POST['book'] . '\'') as $row)
+    foreach ($db->query('SELECT * FROM scriptures WHERE scriptures_id =\'' . $_GET['id'] . '\'') as $row)
     {
     echo "<strong>" . $row['scriptures_book'] . " " . $row['scriptures_chapter'] . ":" . $row['scriptures_verse'] . " - </strong>";
-    echo "<a href='scriptureDetails.php?id=". $row['scriptures_id'] . "'> 'Scripture Details' </a>";
+    echo "\"" . $row['scriptures_content'] . "\"";
     echo '<br/>';
     }
 }
