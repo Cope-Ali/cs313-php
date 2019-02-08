3d4a0129b1ad57bc3727d9d853ab0a30c41d92a8
Ali Cope <35251289+Cope-Ali@users.noreply.github.com>
2019-02-07 21:55:12 -0800

    syntax error

--- web/oppCenter/personDetails.php
+++ web/oppCenter/personDetails.php
@@ -38,7 +38,7 @@ if($_GET['id'] != "")
 
     foreach ($db->query('SELECT * FROM progress WHERE progress_person =\'' . $_GET['id'] . '\'') as $row)
     {
-    echo "Current Opportunities: <br>" . $db->query('SELECT * FROM opportunities WHERE opportunity_id=\'' . $row['progress_opportunity'] . '\'') as $row)
+    echo "Current Opportunities: <br>" . ($db->query('SELECT * FROM opportunities WHERE opportunity_id=\'' . $row['progress_opportunity'] . '\'') as $row)
     echo "<br> Status: " . $row['progress_status'] . '<br>';
         foreach ($db->query('SELECT * FROM notes WHERE notes_progress =\'' . $row['progress_id'] . '\'') as $row)
         {
