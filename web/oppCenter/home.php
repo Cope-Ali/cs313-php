8c98d88d5c8e8038a6c00447690b0f161700877a
Ali Cope <35251289+Cope-Ali@users.noreply.github.com>
2019-02-07 19:50:35 -0800

    move db access script to seperate page

--- web/oppCenter/home.php
+++ web/oppCenter/home.php
@@ -2,28 +2,8 @@
 <?php
 //start session to store credentials
 session_start();
-try
-{
-  $dbUrl = getenv('DATABASE_URL');
 
-  $dbOpts = parse_url($dbUrl);
-
-  $dbHost = $dbOpts["host"];
-  $dbPort = $dbOpts["port"];
-  $dbUser = $dbOpts["user"];
-  $dbPassword = $dbOpts["pass"];
-  $dbName = ltrim($dbOpts["path"],'/');
-
-  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-
-  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $ex)
-{
-  echo 'Error!: ' . $ex->getMessage();
-  die();
-}
-
+include("accessDB.php");
 ?>
 
 <!DOCTYPE html>
