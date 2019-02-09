1aa93711283c5b924e815fd24301a3c5861b889c
Ali Cope <35251289+Cope-Ali@users.noreply.github.com>
2019-02-08 16:07:23 -0800

    fix colorig

--- web/oppCenter/nav.php
+++ web/oppCenter/nav.php
@@ -1,19 +1,19 @@
 <style>
-.nav{
+#nav{
     font-family: 'Open Sans', sans-serif;
     background-color: #333333;
     color: white;
     text-align: center;
 }
 
-a .nav{
+a#nav{
     color: white;
     padding: 40px;
 }
 
 </style>
 
-<div class="nav">
+<div id="nav">
   <a href="/home.php">Home</a>
   <a href="/newForm.php">Add New</a>
 </div>
