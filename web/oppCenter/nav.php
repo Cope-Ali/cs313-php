018d8f1c1ad66dd407c70805204b8e4e99560163
Ali Cope <35251289+Cope-Ali@users.noreply.github.com>
2019-02-08 16:03:09 -0800

    create nav and newForm

---
+++ web/oppCenter/nav.php
@@ -0,0 +1,17 @@
+<style>
+.nav{
+    font-family: 'Open Sans', sans-serif;
+    background-color: #333333;
+    color: white;
+    text-align: center;
+}
+
+
+
+</style>
+
+<div class="nav">
+  <a href="/home.php">Home</a>
+  <a href="/newForm.php">Add New</a>
+</div>
+
---
+++ web/oppCenter/newForm.php
@@ -0,0 +1,26 @@
+<?php
+//start session to store credentials
+session_start();
+include("accessDB.php");
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <?php include("header.php");?>
+    
+    <link rel="stylesheet" href="oppStyle.css">
+</head>
+<body>
+
+<?php include("nav.php");?>
+
+<div class="container" >
+<h2> Content Coming Soon </h2>
+</div>
+
+<?php include("footer.php");?>
+
+</body>
+</html>
