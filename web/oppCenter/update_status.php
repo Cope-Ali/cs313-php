6e65327ae122a134670a6046991d94ae5a626688
Ali Cope <35251289+Cope-Ali@users.noreply.github.com>
2019-02-21 08:37:54 -0800

    debugging

--- web/oppCenter/update_status.php
+++ web/oppCenter/update_status.php
@@ -8,6 +8,8 @@ $person_id = htmlspecialchars($_POST['person_id']);
 $progress_id = htmlspecialchars($_POST['progress_id']);
 $status =  htmlspecialchars($_POST['status']);
 
+echo $person_id . " " . $progress_id . " " . $status;
+
 $stmt = $db->prepare('Update progress
     SET progress_status = :status
     WHERE progress_id = :progress:id;');
@@ -15,7 +17,7 @@ $stmt->bindValue(':progress_id', $progress_id, PDO::PARAM_INT);
 $stmt->bindValue(':status', $status, PDO::PARAM_STR);
 $stmt->execute();
 
-$new_page ="personDetails.php?id=$person_id";
-header("Location: $new_page");
-die();
+// $new_page ="personDetails.php?id=$person_id";
+// header("Location: $new_page");
+// die();
 ?>
