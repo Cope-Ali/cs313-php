d959e93d3640f2e07579732cc6e95e73550cd266
Ali Cope <35251289+Cope-Ali@users.noreply.github.com>
2019-02-21 08:06:29 -0800

    fix formatting and add Logout

--- web/oppCenter/signin.php
+++ web/oppCenter/signin.php
@@ -9,10 +9,13 @@ include("accessDB.php");
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="UTF-8">
+    <?php include("header.php");?>
+    
+    <link rel="stylesheet" href="oppStyle.css">
 </head>
 <body>
+<h1> Please sign in: </h1>
 
-<a href="signup.php">Sign up now!</a> <br>
 
 <?php
     if($_GET['error'] == "true") {
@@ -25,5 +28,7 @@ include("accessDB.php");
     <input name="password" placeholder="Password">
     <input type="submit" value="Submit">
 </form>
+<h2>New to the site? </h2>
+<a href="signup.php">Sign up now!</a> <br>
 </body>
 </html>
